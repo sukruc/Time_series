09d5408425bbae00d2bcce696dc38358ee4aeefb
sukruc <39256971+sukruc@users.noreply.github.com>
2018-09-12 11:30:32 -0700

    example updated

--- example_garch_generator.py
+++ example_garch_generator.py
@@ -19,7 +19,7 @@ b1 = 0.3
 b2 = 0.15
 n = 10000
 
-garch_generator(n=n,a0=a0,a1=a1,a2=a2,b1=b1,b2=b2)
+sigsq,eps = garch_generator(n=n,a0=a0,a1=a1,a2=a2,b1=b1,b2=b2)
 #sigsq,eps= garch_sample_generator(n=n,a={'0':a0,'1':a1},b={'1':b1},seed=2)
 
 tsplot(eps, lags=30)
@@ -28,7 +28,7 @@ plt.savefig('garch_model_tsa.png',format='png')
 tsplot(eps**2,lags=30)
 plt.savefig('garch_model_epssq.png',format='png')
 
-#Fit a GARCH(1,1) model to simulated EPS series
+#Fit a GARCH(2,2) model to simulated EPS series
 #sigsq,eps= garch_sample_generator(n=n,a={'0':a0,'1':a1},b={'1':b1},seed=2)
 
 am = arch_model(eps, p=2, o=0, q=2, dist='StudentsT')
